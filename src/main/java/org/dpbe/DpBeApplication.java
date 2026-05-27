6c3840e39f730fc95320e1b400db80ee5ed98831
enohs <kmsn01@naver.com>
2026-05-28 00:58:13 +0900

    fix: 프로젝트 재생성

---
+++ src/main/java/org/dpbe/DpBeApplication.java
@@ -0,0 +1,13 @@
+package org.dpbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DpBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/dpbe/DpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dpbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
